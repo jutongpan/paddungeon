426f6b1c0bf67a4d963c67fd10efd7ca63e5a4c3
Jutong Pan <panjutong@gmail.com>
2019-06-29 02:58:39 +0000

    Added ReverseProxied class to fix url issues in nginx deployment on subpath.

--- app.py
+++ app.py
@@ -4,7 +4,25 @@ import pandas as pd
 import re
 import urllib.request
 
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 df_dungeon = pd.read_csv("dungeon.csv")
 list_dungeonName = df_dungeon.dungeonName.tolist()
@@ -16,7 +34,7 @@ def index():
         list_dungeonName = list_dungeonName
         )
 
-@app.route('/paddungeon/<dungeon>', methods=['GET', 'POST'])
+@app.route('/<dungeon>', methods=['GET', 'POST'])
 def dungeonPage(dungeon):
     if request.method == 'POST':
         return redirect(url_for('index'))
